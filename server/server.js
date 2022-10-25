e71e958da1290fe92f74ae1efd0fd8892c596a32
RichardMartinAUS <richard.martin001@gmail.com>
2022-10-25 20:12:18 +1100

    test deploy

--- server/server.js
+++ server/server.js
@@ -36,9 +36,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
+      // console.log(
+      //   `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      // );
     });
   });
 };

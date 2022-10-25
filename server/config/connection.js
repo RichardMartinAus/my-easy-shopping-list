514870c72ddcecf3dfa074c19c60b2142d40c5ad
RichardMartinAUS <richard.martin001@gmail.com>
2022-10-25 20:33:04 +1100

    test deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/myeasyshoppinglist',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
